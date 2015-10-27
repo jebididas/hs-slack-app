c1987281051e4c57741d82bbc85e92afe02d19bb
Jody Chambers <jodyc@172-18-137-197.ip.hootsuitemedia.com>
2015-10-27 11:46:18 -0700

    added write to file

--- uninstall.php
+++ uninstall.php
@@ -1,40 +1,17 @@
 
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Uninstall</title>
-	<script type="text/javascript">
-
 		<?php
 
 		$memberId = $_REQUEST['member_id'];
 		$userId = $_REQUEST['user_id'];
 
-		function debug_to_console( $data ) {
-
-		    if ( is_array( $data ) )
-		        $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
-		    else
-		        $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
-
-		    echo $output;
-		}
-
-		debug_to_console( );
-		debug_to_console( "User ID: " + $userId );
-
+		$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+		$txt = $userId + " - USER \n";
+		fwrite($myfile, $txt);
+		$txt = $memberId + " - MEMBER \n";
+		fwrite($myfile, $txt);
+		fclose($myfile);
 
 
 		?>
 
-		console.log(<?php echo "Member ID: " + $memberId; ?>);
-		console.log(<?php echo "TEST"; ?>);
-
-	</script>
-</head>
-<body>
-
-</body>
-</html>
 
