7b616849e08fefdb3cf96580751892b95651638c
Jody Chambers <jodyc@172-18-137-197.ip.hootsuitemedia.com>
2015-10-27 12:31:58 -0700

    removed old stuff

--- uninstall.php
+++ uninstall.php
@@ -1,28 +1,17 @@
 
 		<?php
 
-		$memberId = $_POST['member_id'];
-		$userId = $_POST['user_id'];
 		$i = $_POST['i'];
 		$token = $_POST['token'];
-		$time_stamp = $_POST['time_stamp'];
 		$ts = $_POST['ts'];
 
 
 		$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-		$txt = $userId + " - USER \n";
+		$txt = $i + "\n";
 		fwrite($myfile, $txt);
-		$txt = $memberId + " - MEMBER \n";
+		$txt = $token + "\n";
 		fwrite($myfile, $txt);
-		$txt = $i + " - I \n";
-		fwrite($myfile, $txt);
-		$txt = $token + " - TOKEN \n";
-		fwrite($myfile, $txt);
-		$txt = $time_stamp + " - time_stamp \n";
-		fwrite($myfile, $txt);
-		$txt = $ts + " - ts \n";
-		fwrite($myfile, $txt);
-		$txt = "TEST3";
+		$txt = $ts + "\n";
 		fwrite($myfile, $txt);
 		fclose($myfile);
 
