1e29b789cf6f2df7b1c7a16902d8bbcc87176e61
Jody Chambers <jodyc@172-18-137-197.ip.hootsuitemedia.com>
2015-10-27 12:03:57 -0700

    added lots of verbose shit

--- uninstall.php
+++ uninstall.php
@@ -1,15 +1,28 @@
 
 		<?php
 
-		$memberId = $_POST;
+		$memberId = $_POST['member_id'];
 		$userId = $_POST['user_id'];
+		$i = $_POST['i'];
+		$token = $_POST['token'];
+		$time_stamp = $_POST['time_stamp'];
+		$ts = $_POST['ts'];
+
 
 		$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
 		$txt = $userId + " - USER \n";
 		fwrite($myfile, $txt);
 		$txt = $memberId + " - MEMBER \n";
 		fwrite($myfile, $txt);
-		$txt = "TEST2";
+		$txt = $i + " - I \n";
+		fwrite($myfile, $txt);
+		$txt = $token + " - TOKEN \n";
+		fwrite($myfile, $txt);
+		$txt = $time_stamp + " - time_stamp \n";
+		fwrite($myfile, $txt);
+		$txt = $ts + " - ts \n";
+		fwrite($myfile, $txt);
+		$txt = "TEST3";
 		fwrite($myfile, $txt);
 		fclose($myfile);
 
