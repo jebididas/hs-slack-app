ea39fae2902114b8758c839731663ff3602b793f
Jody Chambers <jodyc@jody-chambers.local>
2015-10-30 14:46:40 -0700

    added auth workflow

--- hsapp.js
+++ hsapp.js
@@ -1,7 +1,8 @@
+var messageId = "messageId=" + encodeURIComponent(message.post.id);         
+var datetime  = "&datetime=" + encodeURIComponent(message.post.datetime);
+var message   = "&message=" + encodeURIComponent(message.post.content.body);
+
 function sendToAppHandler(message) {
-  var messageId = "messageId=" + encodeURIComponent(message.post.id);            // Check to see if this is ID or Id
-  var datetime  = "&datetime=" + encodeURIComponent(message.post.datetime);
-  var message   = "&message=" + encodeURIComponent(message.post.content.body);
   var handler = 'https://hs-slack.herokuapp.com/handler.html?' + messageId + message + datetime;
 
   hsp.showCustomPopup(handler, 'Send to Slack Channel');
@@ -12,10 +13,38 @@ $(document).ready(function() {
         useTheme: true
     });
 
+    // Check if message has been stored already
+    if (localstorage.getItem('hs_message_id') == undefined){
+        console.log('Message has not been stored in localstorage yet');
+        // Store message details in storage before auth check
+        localstorage.setItem('hs_message_id', messageId);
+        localstorage.setItem('hs_datetime', datetime);
+        localstorage.setItem('message', message);
+    }else{
+        console.log('Message has been stored in localstorage');
+        messageId = localstorage.getItem('hs_message_id');
+        datetime = localstorage.getItem('datetime');
+        message = localstorage.getItem('message');
+    }
 
-    hsp.bind('sendtoapp', function(message){
-        sendToAppHandler(message);
-    });
+    // Check if user has been authed
+    if (localstorage.getItem('slack_team_token') != undefined){
+        console.log('Slack token found - user has authed');
+        // Clear message from storage
+        localstorage.removeItem('hs_message_id');
+        localstorage.removeItem('hs_datetime');
+        localstorage.removeItem('message');
+
+        // Send message to plugin modal window
+        hsp.bind('sendtoapp', function(message){
+            sendToAppHandler(message);
+        });
+    }else{
+        console.log('Slack token NOT found - user has NOT authed yet');
+        window.location.reload('login.html');
+    }
+
+
 
 
     // hsp.bind('sendtoapp', function(message){
