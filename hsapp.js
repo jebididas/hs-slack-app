fc812a71ee288f3fd3a2d07ecc3a355a2479d7f8
Jody Chambers <jodyc@jody-chambers.local>
2015-10-30 20:46:09 -0700

    style changes to team dropdown

--- hsapp.js
+++ hsapp.js
@@ -16,13 +16,11 @@ function sendToAppHandler(message) {
 
     // Check if message has been stored already
     if (localStorage.getItem('hs_message_id') == undefined){
-        console.log('Message has not been stored in localStorage yet');
         // Store message details in storage before auth check
         localStorage.setItem('hs_message_id', hs_message_id);
         localStorage.setItem('hs_datetime', hs_datetime);
         localStorage.setItem('hs_message', hs_message);
     }else{
-        console.log('Message has been stored in localStorage');
         hs_message_id = localStorage.getItem('hs_message_id');
         hs_datetime = localStorage.getItem('hs_datetime');
         hs_message = localStorage.getItem('hs_message');
@@ -32,39 +30,16 @@ function sendToAppHandler(message) {
 }
 
 $(document).ready(function() {
+
     hsp.init({
         useTheme: true
     });
 
     // Send message to plugin modal window
     hsp.bind('sendtoapp', function(message){
-        var pid = getParameterByName('pid');
-        console.log('PID:');
-        console.log(pid);
-        localStorage.setItem('pid',pid);
+        localStorage.setItem('pid',getParameterByName('pid');
         console.log(message);
         sendToAppHandler(message);
     });
 
-
-
-
-    // hsp.bind('sendtoapp', function(message){
-    //     var userid = message.post.user.userid;
-    //     var username = message.post.user.username;
-    //     var avatar;
-    //     if (message.post.network == 'twitter') {
-    //         avatar = 'http://avatars.io/twitter/' + username;
-    //     } else {
-    //         avatar = 'http://avatars.io/facebook/' + userid;
-    //     }
-        
-
-    //     $('.hs_topBar').after(messageTemplate({
-    //         input: message.post.content.body,
-    //         username: username,
-    //         avatar: avatar,
-    //         messageId: parseInt(1000000*Math.random(), 10) //generate random message ID for demonstration purposes
-    //     }));
-    // });
 });
