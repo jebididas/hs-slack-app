d438391a32318f7c5bb0c9c6a35d9b4eda3805e3
Jody Chambers <jodyc@jody-chambers.local>
2015-11-07 23:45:27 -0800

    db test

---
+++ db-connection-creds.php
@@ -0,0 +1,6 @@
+<php
+	$dbhost = "hs-slack-db.herokuapp.com";
+	$dbname = "hs-slack-db";
+	$dbuser = "jody.chambers@hootsuite.com";
+	$dbpassword = "gr8fulowl";
+?>
--- uninstall.php
+++ uninstall.php
@@ -1,18 +1,39 @@
 <?php
+	require_once('db-connection-creds.php');
 
-	$i = $_POST['i'];
-	$ts = $_POST['ts'];
-	$token = $_POST['token'];
+	// $i = $_POST['i'];
+	// $ts = $_POST['ts'];
+	// $token = $_POST['token'];
 	
 
 
-	$myfile = fopen("uninstall_list.csv", "w") or die("Unable to open file!");
-	$headers = "hs_member_id,timestamp,token";
-	fwrite($myfile, $headers);
-	$txt = $i . "," . $ts . "," . $token;
-	fwrite($myfile, $txt);
-	fclose($myfile);
+	// $myfile = fopen("uninstall_list.csv", "w") or die("Unable to open file!");
+	// $headers = "hs_member_id,timestamp,token";
+	// fwrite($myfile, $headers);
+	// $txt = $i . "," . $ts . "," . $token;
+	// fwrite($myfile, $txt);
+	// fclose($myfile);
 
+	// Connect to db
+	$dbc = pg_connect("host=" . $dbhost . " dbname=" . $dbname . " user=" . $dbuser . " password=" . $dbpassword);
+
+	if(!$dbc){
+    die("Error in connection: " . pg_last_error());
+	}else{
+		echo "DB connected!";
+	}
+
+	// Write to db
+	// $sql = "SELECT * FROM Countries";
+	// $result = pg_query($dbc, $sql);
+
+	// if (!$result) {
+	//   die("Error in SQL query: " . pg_last_error());
+	// }
+
+	// // free memory
+	// pg_free_result($result);
+
+	// close connection
+	pg_close($dbc);
 ?>
-
-
