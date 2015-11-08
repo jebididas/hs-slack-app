b2be61b671363d30c5b31163666ca4d91158e2c5
Jody Chambers <jodyc@jody-chambers.local>
2015-11-08 15:13:58 -0800

    v

--- hsapp.js
+++ hsapp.js
@@ -9,7 +9,7 @@ function sendToAppHandler(message) {
     var hs_message_id = message.post.id;
     var hs_datetime = message.post.datetime;
     var hs_message = message.post.content.body; // Not sure if we need the other variables below to be sent to handler
-    var hs_username = message.post.user.username;
+    var hs_username = message.profile.screen_name;
     var messageId = "messageId=" + encodeURIComponent(hs_message_id);         
     var datetime = "&datetime=" + encodeURIComponent(hs_datetime);
     var message = "&message=" + encodeURIComponent(hs_message);
