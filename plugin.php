3880f218f470ba9cf7abc3f3eda6daecbc8aefbf
Jody Chambers <jodyc@172-18-137-197.ip.hootsuitemedia.com>
2016-03-01 17:22:48 -0800

    Swapped back to 2.0 js

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 <head>
   <script type="text/javascript" src="js/jquery-2.2.1.min.js"></script>
   <script type="text/javascript" src="js/sha512.js"></script>
-  <script src="https://assets.hootsuite.com/appdirectory/libs/3.0/hs-sdk.js"></script>
+  <script src="https://d2l6uygi1pgnys.cloudfront.net/jsapi/2-0/hsp.js"></script>
 </head>
 <body>
 	<script type="text/javascript" src="js/plugin.js"></script>
